cf76c803b896fb9f4187243449b62e0657f3d181
Reeves Little <ReevesL@users.noreply.github.com>
2019-09-03 21:43:02 -0700

    Basic files added
    First set of working files

---
+++ src/server_auto_update.ps1
@@ -0,0 +1,19 @@
+﻿#
+# server_auto_update.ps1
+# basic powershell script to drive updating *nix consolues via putty's plink command line
+#
+$serverList = @()
+$serverList += "Unifi2"
+$serverList += "Plex"
+$serverList += "Grafana"
+$serverList += "Mr Fix-it"
+#$serverList += "myPhpFaq"
+
+# Todo: add progress bar showing current server and overall progress
+# Todo: change the config test file to a relative path
+foreach ($serverName in $serverList) {
+	Write-Host "Updating " $serverName
+	& plink -v -ssh -t -m "c:\test\update.txt" -load $serverName 
+
+
+}
